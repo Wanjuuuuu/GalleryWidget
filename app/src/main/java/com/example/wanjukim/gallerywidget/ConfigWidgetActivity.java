a28bbf27dde1467b85b5ebd03109ca0ac01a0f2f
Wanjuuuuu <makarong5798@gmail.com>
2017-07-12 17:38:34 +0900

    changed widget_cofig_main.xml and add new configWidgetActivity

---
+++ app/src/main/java/com/example/wanjukim/gallerywidget/ConfigWidgetActivity.java
@@ -0,0 +1,17 @@
+package com.example.wanjukim.gallerywidget;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Wanju Kim on 2017-07-12.
+ */
+
+public class ConfigWidgetActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.widget_config_main);
+    }
+}
